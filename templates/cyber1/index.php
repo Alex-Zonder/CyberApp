db8277bdbca3d703e48f9cae353716bd82a220e1
Zonder <f-m@bk.ru>
2019-11-22 00:23:18 +0300

    Added images & icons

--- templates/cyber1/index.php
+++ templates/cyber1/index.php
@@ -1,23 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<?php /* Head */ ?>
+	<?php /*                            Head                                  */ ?>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title><?php echo $title; ?></title>
+
+	<link rel="shortcut icon" href="/public/images/favicon/favicon.gif" type="image/x-gif">
+	<link rel="apple-touch-icon" href="/public/images/favicon/iphone_icon.png">
 
 	<?php /*   Scripts   */ ?>
 	<script src="/public/scripts/useragent.js"></script>
 	<script src="/public/scripts/json.js"></script>
 	<script src="/public/scripts/fullscreen.js"></script>
 
-	<?php /*   Template   */ ?>
+	<?php /*                            Template                              */ ?>
 	<link rel="stylesheet" type="text/css" href="/templates/cyber1/css/main.css" />
 	<script src="/templates/cyber1/js/main.js"></script>
 </head>
 <body>
 	<?php /*   Header   */ ?>
-	<div class='header'>
-		<div class='float_left'>Меню</div>
+	<div class='header' id="syteHeader">
+		<div class='float_left'><img src="/templates/cyber1/icons/menu.png" style="height:28px;"></div>
 		<div class='float_right'>
 			<?php if (isset($user['name'])): ?>
 				<?php echo $user['name']; ?>
@@ -29,8 +32,8 @@
 		<div class='float_clear'></div>
 	</div>
 
-	<?php /*   Left Menu   */ ?>
-	<div class='left_menu'>
+	<?php /*                           Left Menu                             */ ?>
+	<div class='left_menu' id="syteLeftMenu">
 		<a href="/">Главная</a>
 		<?php if (isset($user['name'])): ?>
 			<?php if (isset($user['group']) && $user['group'] == 'admin'): ?>
@@ -43,13 +46,13 @@
 		<?php endif; ?>
 	</div>
 
-	<?php /*   Content   */ ?>
-	<div class='content'>
+	<?php /*                             Content                              */ ?>
+	<div class='content' id="syteContent">
 		<?php echo $content; ?>
 	</div>
 
-	<?php /*   Footer   */ ?>
-	<div class='float_clear'></div>
+	<?php /*                             Footer                               */ ?>
+	<div class='float_clear' id="syteFooter"></div>
 	<div class='footer'>
 		<p style="text-align: center; margin: 5px 0;">
 			&copy; <a href="http://cyber-light.ru/" target="_blank">Киберлайт</a> <?php echo date("Y");?> | Все права защищены.
